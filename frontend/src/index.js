9c72fb27b77540852e96c3e2e75ccf56f3292b23
alexabraham0504 <alexabraham2025@mca.ajce.in>
2024-11-06 12:07:03 +0530

    deploy 16

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.min.js";
 // import '@fortawesome/fontawesome-free/css/all.min.css';  // Import Font Awesome styles
-import 'bootstrap-icons/font/bootstrap-icons.css';  // Import Bootstrap Icons styles
+// import 'bootstrap-icons/font/bootstrap-icons.css';  // Import Bootstrap Icons styles
 
 // Import custom CSS files  
 
